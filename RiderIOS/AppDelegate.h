2e43b4e221255b04e656ba2f2db9e6404712d14f
mac <15152479116@163.com>
2018-06-04 10:25:01 +0800

    Initial Commit

---
+++ RiderIOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RiderIOS
+//
+//  Created by Han on 2018/6/4.
+//  Copyright © 2018年 Han. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
